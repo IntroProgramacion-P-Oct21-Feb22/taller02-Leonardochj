b0ac6508aeeb679ff242a4eb1aed4695d35ac20a
Leonardo Chuquimarca <hlchuquimarca@utpl.edu.ec>
2021-10-22 11:39:05 -0500

    primer commit

--- proyecto02/src/proyecto02/Proyecto02.java
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -15,6 +15,9 @@ public class Proyecto02 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Ejemplo básico de Java");
+        System.out.println("Versionado a través de Git\n");
+        System.out.println("Manejado desde GitHub");
     }
     
 }
